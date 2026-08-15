e46921a64f510c50ce6a83ae14f3d517ac0274a5
maxedmunds2011 <max.edmunds1@education.nsw.gov.au>
2026-08-15 11:38:55 +1000

    Added functions for main.py, ready to begin work on the LEDs and how temperature and moisture affect them.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Add imports here (machine, time, etc.)
-import machine
-import time
+from machine import Pin, ADC # Connects the Raspberry Pi pins to Thonny
+from time import sleep # Allows for the use of the sleep() function
+
 
 # Add variables here (temperature_control, moisture_control, led_control, buzzer_control, etc.)
 temperature_control = True
@@ -8,6 +9,21 @@ moisture_control = True
 led_control = True
 buzzer_control = True
 
+
+def too_high():
+
+
+def warning():
+
+
+def just_right():
+
+
+def too_low():
+
+
+def clap_detect():
+
+
 def main():
     while True:
-        print()
